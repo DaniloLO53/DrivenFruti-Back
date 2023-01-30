4eb1514f55ddb77d55d847b01c5d6bd23cae3b1e
crixstn <crixstn@gmail.com>
2023-01-30 11:31:27 -0300

    sign up complete

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,9 +3,11 @@ import { signIn } from "../controllers/signInControllers.js";
 import { signOut } from "../controllers/signOut.controller.js";
 import { getCart, updateCart } from "../controllers/users.controller.js";
 import { signInValidation } from "../middlewares/signInMiddlewares.js";
+import { signUp } from "../controllers/signUp.js";
 
 const router = Router();
 
+router.post("/sign-up", signUp);
 router.post("/sign-in", signInValidation, signIn);
 router.put("/cart", updateCart);
 router.get("/cart", getCart);
